f19e2b19393fa407946d0d64c0bc41def90dfae9
lavanya <cslavanya1199@gmail.com>
2025-10-22 12:31:26 +0530

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import MyContext from './myCongtext/MyContext.jsx'
+import { Provider } from 'react-redux';
+import { store } from './reduxToolKit-store/store.js';
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+    <MyContext>
+
+
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider >
+
+      </BrowserRouter>
+    </MyContext>
   </StrictMode>,
 )
